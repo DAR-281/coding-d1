76a457a255d4d9dd1999ad1aa77a9756d9431a9b
DAR-281 <isohu2019@gmail.com>
2025-06-26 19:49:08 -0700

    Instructions D2

---
+++ d2/Main.java
@@ -0,0 +1,21 @@
+public class Main{
+
+public static void main(String[] args) {
+
+System.out.println("My magie recipie..");
+
+maggie();
+
+}
+
+public static void maggie() {
+
+System.out.println("1.Boil water in a pan...");
+
+System.out.println("2.Now add special ingrediants and cook for 2 mins..");
+
+System.out.println("2. sever hot..");
+
+}
+
+}
---
+++ d2/operator.java
@@ -0,0 +1,13 @@
+public class operator {
+    public static void main(String[] args) {
+        int a=20;
+        int b=22;
+System.out.println("==================Guess Num=================");
+System.out.println("Uniary operator"+(a+b));
+System.out.println("Uniart Operator"+(++b));
+System.out.println("Binary Op"+(-a+b));
+System.out.println("Terniery operartor"+((a-b)+a-b));
+System.out.println("===========================================");
+    }
+}
+
