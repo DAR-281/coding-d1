1fac66f9435a31fab5b52dffb188b7e5c53f2424
DAR-281 <isohu2019@gmail.com>
2025-07-04 18:42:48 -0700

    even odd hw 3

--- Day 3/day3hw.java
+++ Day 3/day3hw.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
+
 public class day3hw {
-    
-}
+
+     public static void main(String[] args) {
+    Scanner scn=new Scanner(System.in);
+
+    int x=scn.nextInt();
+
+    if ((x % 2) == 0){
+        System.out.println("This is a even number");
+    } else {
+System.out.println("Odd Number");
+
+
+
+
+    }
+
+     }
+    }
